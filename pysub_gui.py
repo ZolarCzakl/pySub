8a0437224eb122a4cf7eecb273201ae243a5dcf8
ZolarCzakl <rod.cat@free.fr>
2017-05-19 17:22:33 +0200

    amélioration de la GUI

--- pysub_gui.py
+++ pysub_gui.py
@@ -19,9 +19,17 @@ class Assos:
         self.sub =sub
         self.dem = dem
 
+def verif(liste, nom):
+    for asso in liste:
+        if asso.nom == nom:
+            vrai = 1
+            break
+        else:
+            vrai = 0
+    return vrai
 
 def nouvelle_asso():
-    nom = entr1.get()
+    nom = (entr1.get()).lower()
     nbadh = int(entr2.get())
     adh = float(entr3.get())
     tres = float(entr4.get())
@@ -31,18 +39,24 @@ def nouvelle_asso():
     dem = float(entr8.get())
     asso = Assos(nom, nbadh, adh, tres, sal, loc, sub, dem)
     try:
-        fichAssos = open("assos", 'rb')        
-        listAssos = pickle.load(fichAssos)        
-        listAssos.append(asso)
+        fichAssos = open("assos", 'rb')
+        listAssos = pickle.load(fichAssos)
         fichAssos.close()
-        fichAssos = open("assos", 'wb')
+        if verif(listAssos, nom):
+            confirm.configure(text = "L'association " + nom + " est déjà enregistrée")
+        else:
+            listAssos.append(asso)
+            fichAssos = open("assos", 'wb')
+            pickle.dump(listAssos, fichAssos)
+            fichAssos.close()
+            confirm.configure(text = "L'association " + asso.nom + " est bien enregistrée")
         
     except:
         fichAssos = open("assos", 'wb')
-        listAssos = [asso]        
-    pickle.dump(listAssos, fichAssos)
-    fichAssos.close()
-    confirm.configure(text = "L'association " + asso.nom + " est bien enregistrée")
+        listAssos = [asso]
+        pickle.dump(listAssos, fichAssos)
+        fichAssos.close()
+        confirm.configure(text = "L'association " + asso.nom + " est bien enregistrée")
     
 def listing():
     try:
@@ -57,7 +71,7 @@ def listing():
         
         
     except:
-        listing.configure(text = "Fichier vide")
+        liste.configure(text = "Fichier vide")
     
     
 def vidage():
@@ -73,14 +87,14 @@ def vidage():
 
 fen = Tk()
 
-Label(fen, text = "Association: ").grid(row = 0, columnspan = 2, sticky = W)
-Label(fen, text = "Nombre d'adhérents: ").grid(row = 1, columnspan = 2, sticky = W)
-Label(fen, text = "Montant de l'adhésion: ").grid(row = 2, columnspan = 2, sticky = W)
-Label(fen, text = "Trésorerie: ").grid(row = 3, columnspan = 2, sticky = W)
-Label(fen, text = "Salarié(es): ").grid(row = 4, columnspan = 2, sticky = W)
-Label(fen, text = "Occupation des salles: ").grid(row = 5, columnspan = 2, sticky = W)
-Label(fen, text = "Subventions extérieures: ").grid(row = 6, columnspan = 2, sticky = W)
-Label(fen, text = "Montant de la demande: ").grid(row = 7, columnspan = 2, sticky = W)
+Label(fen, text = "Association: ").grid(row = 0, sticky = W)
+Label(fen, text = "Nombre d'adhérents: ").grid(row = 1, sticky = W)
+Label(fen, text = "Montant de l'adhésion: ").grid(row = 2, sticky = W)
+Label(fen, text = "Trésorerie: ").grid(row = 3, sticky = W)
+Label(fen, text = "Salarié(es): ").grid(row = 4, sticky = W)
+Label(fen, text = "Occupation des salles: ").grid(row = 5, sticky = W)
+Label(fen, text = "Subventions extérieures: ").grid(row = 6, sticky = W)
+Label(fen, text = "Montant de la demande: ").grid(row = 7, sticky = W)
 entr1 = Entry(fen)
 entr2 = Entry(fen)
 entr3 = Entry(fen)
@@ -103,5 +117,5 @@ Button(fen, text = "Listing", command = listing).grid(row = 8, column = 2, stick
 confirm = Label(fen)
 confirm.grid(row = 9)
 liste = Label(fen)
-liste.grid(row = 10)
+liste.grid(row = 9, column = 1)
 fen.mainloop()
