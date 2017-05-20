67c95d8500fe363988e479bde15647fac9065c36
ZolarCzakl <rod.cat@free.fr>
2017-05-20 09:37:32 +0200

    Ajout du calcul de répartition

--- pysub_gui.py
+++ pysub_gui.py
@@ -1,3 +1,12 @@
+"""
+Aide au calcul de la répartition des subventions de la mairie pour les associations.
+
+@auteur: Rod <rod.cat@free.fr>  License libre
+"""
+
+
+
+
 from tkinter import *
 import pickle
 
@@ -20,6 +29,9 @@ class Assos:
         self.dem = dem
 
 def verif(liste, nom):
+    """
+    Vérifie la présence d'une association dans le fichier.
+    """
     for asso in liste:
         if asso.nom == nom:
             vrai = 1
@@ -29,6 +41,9 @@ def verif(liste, nom):
     return vrai
 
 def nouvelle_asso():
+    """
+    Ajoute une nouvelle association au fichier.
+    """
     nom = (entr1.get()).lower()
     nbadh = int(entr2.get())
     adh = float(entr3.get())
@@ -59,13 +74,16 @@ def nouvelle_asso():
         confirm.configure(text = "L'association " + asso.nom + " est bien enregistrée")
     
 def listing():
+    """
+    Liste les associations déjà enregistrées.
+    """
     try:
         fichAssos = open("assos", 'rb')        
         listAssos = pickle.load(fichAssos)        
         fichAssos.close()
         contenu = ""
         for asso in listAssos:
-            contenu += asso.nom+", " + str(asso.nbadh) + " adhérents" + "\n"
+            contenu += asso.nom + ", " + str(asso.nbadh) + " adhérents" + "\n"
         liste.configure(text = contenu)
             
         
@@ -75,6 +93,9 @@ def listing():
     
     
 def vidage():
+    """
+    Vide les champs du formulaire.
+    """
     entr1.delete(0, END)
     entr2.delete(0, END)
     entr3.delete(0, END)
@@ -83,7 +104,34 @@ def vidage():
     entr6.delete(0, END)
     entr7.delete(0, END)
     entr8.delete(0, END)
+    entr9.delete(0, END)
         
+def coef():
+    """ 
+    Calcul du pourcentage affecté à chaque association
+    """
+    try:
+        fichAssos = open("assos", 'rb')
+        listAssos = pickle.load(fichAssos)
+        fichAssos.close()        
+        dic = {}
+        total = 0
+        contenu = ""
+        try:
+            sub = float(entr9.get())
+            for asso in listAssos:
+                dic[asso.nom] = asso.nbadh +asso.adh +asso.tres +asso.sal -asso.loc -asso.sub +asso.dem
+                total += dic[asso.nom]
+            for k in dic:
+                pourcent = dic[k]/(total/100)
+                assob = sub/100*pourcent
+                contenu += k + " " + str(pourcent) + "% = " + str(assob) + "€\n"
+            liste.configure(text = contenu)
+            confirm.configure(text = "Résultat: ")
+        except:
+            confirm.configure(text = "Entrez une somme à répartir")
+    except:
+        confirm.configure(text = "Aucune associations dans le fichier")
 
 fen = Tk()
 
@@ -95,6 +143,7 @@ Label(fen, text = "Salarié(es): ").grid(row = 4, sticky = W)
 Label(fen, text = "Occupation des salles: ").grid(row = 5, sticky = W)
 Label(fen, text = "Subventions extérieures: ").grid(row = 6, sticky = W)
 Label(fen, text = "Montant de la demande: ").grid(row = 7, sticky = W)
+Label(fen, text = "Montant total à répartir").grid(row = 9, sticky = W)
 entr1 = Entry(fen)
 entr2 = Entry(fen)
 entr3 = Entry(fen)
@@ -103,6 +152,7 @@ entr5 = Entry(fen)
 entr6 = Entry(fen)
 entr7 = Entry(fen)
 entr8 = Entry(fen)
+entr9 = Entry(fen)
 entr1.grid(row = 0, column = 1, columnspan = 2)
 entr2.grid(row = 1, column = 1, columnspan = 2)
 entr3.grid(row = 2, column = 1, columnspan = 2)
@@ -111,11 +161,13 @@ entr5.grid(row = 4, column = 1, columnspan = 2)
 entr6.grid(row = 5, column = 1, columnspan = 2)
 entr7.grid(row = 6, column = 1, columnspan = 2)
 entr8.grid(row = 7, column = 1, columnspan = 2)
+entr9.grid(row = 9, column = 1, columnspan = 2)
 Button(fen, text = "Enregistrer", command = nouvelle_asso).grid(row = 8, sticky = W)
 Button(fen, text = "Nouveau", command = vidage).grid(row = 8, column = 1, sticky = W)
 Button(fen, text = "Listing", command = listing).grid(row = 8, column = 2, sticky = W)
+Button(fen, text = "Calcul de la répartition", command = coef).grid(row = 10, sticky = W)
 confirm = Label(fen)
-confirm.grid(row = 9)
+confirm.grid(row = 11)
 liste = Label(fen)
-liste.grid(row = 9, column = 1)
+liste.grid(row = 11, column = 1)
 fen.mainloop()
