8bae2303465089180ad83ee3a729fb715c9b42ed
ZolarCzakl <rod.cat@free.fr>
2017-07-08 16:37:59 +0200

    Ajout de Label sur les curseurs horizontaux

--- pysub_gui.py
+++ pysub_gui.py
@@ -6,7 +6,7 @@ Aide au calcul de la répartition des subventions de la mairie
 """
 
 from tkinter import *
-from tkinter import ttk
+from tkinter.ttk import *
 import pickle
 
 
@@ -111,39 +111,52 @@ def coef():
     except:
         confirm.set("Aucune associations dans le fichier")
 
+def scale_val():
+    val_adherents.set("Adhérents:     " + str(round(adherent.get(),2)))
+    val_adhesion.set("Adhésion:      " + str(round(adhesion.get(),2)))
+    val_tresorerie.set("Trésorerie:         " + str(round(tresorerie.get(),2)))
+    val_salarie.set("Salarié(es):     " + str(round(salarie.get(),2)))
+    val_salle.set("Usage salle:    " + str(round(salle.get(),2)))
+    val_subvention.set("Subvention ext:   " + str(round(subvention.get(),2)))
+
+def scale_update(event):
+    scale_val()
+
 
 root = Tk()
 root.title("Aide à la répartition des subventions")
 
-fen = ttk.Frame(root, padding=10)
+
+fen = Frame(root, padding=10)
 fen.grid(column=0, row=0, sticky=(E,W))
-message = ttk.Frame(root, padding=10, borderwidth=2,
-                    relief="sunken", height=400)
+message = Frame(root, padding=10, borderwidth=2, relief="sunken", height=400)
 message.grid(column=0, row=12, sticky=(E,W))
-scale = ttk.Frame(root, padding=10)
+scale = Frame(root, padding=10)
 scale.grid(column=0, row=20, rowspan=3)
+
 
 confirm = StringVar()
 liste = StringVar()
 
-ttk.Label(fen, text="Association: ").grid(row=0, sticky=W)
-ttk.Label(fen, text="Nombre d'adhérents: ").grid(row=1, sticky=W)
-ttk.Label(fen, text="Montant de l'adhésion: ").grid(row=2, sticky=W)
-ttk.Label(fen, text="Trésorerie: ").grid(row=3, sticky=W)
-ttk.Label(fen, text="Salarié(es): ").grid(row=4, sticky=W)
-ttk.Label(fen, text="Occupation des salles: ").grid(row=5, sticky=W)
-ttk.Label(fen, text="Subventions extérieures: ").grid(row=6, sticky=W)
-ttk.Label(fen, text="Montant de la demande: ").grid(row=7, sticky=W)
-ttk.Label(fen, text="Montant total à répartir").grid(row=9, sticky=W)
-entr1 = ttk.Entry(fen, width=30)
-entr2 = ttk.Entry(fen, width=30)
-entr3 = ttk.Entry(fen, width=30)
-entr4 = ttk.Entry(fen, width=30)
-entr5 = ttk.Entry(fen, width=30)
-entr6 = ttk.Entry(fen, width=30)
-entr7 = ttk.Entry(fen, width=30)
-entr8 = ttk.Entry(fen, width=30)
-entr9 = ttk.Entry(fen, width=30)
+
+Label(fen, text="Association: ").grid(row=0, sticky=W)
+Label(fen, text="Nombre d'adhérents: ").grid(row=1, sticky=W)
+Label(fen, text="Montant de l'adhésion: ").grid(row=2, sticky=W)
+Label(fen, text="Trésorerie: ").grid(row=3, sticky=W)
+Label(fen, text="Salarié(es): ").grid(row=4, sticky=W)
+Label(fen, text="Occupation des salles: ").grid(row=5, sticky=W)
+Label(fen, text="Subventions extérieures: ").grid(row=6, sticky=W)
+Label(fen, text="Montant de la demande: ").grid(row=7, sticky=W)
+Label(fen, text="Montant total à répartir").grid(row=9, sticky=W)
+entr1 = Entry(fen, width=30)
+entr2 = Entry(fen, width=30)
+entr3 = Entry(fen, width=30)
+entr4 = Entry(fen, width=30)
+entr5 = Entry(fen, width=30)
+entr6 = Entry(fen, width=30)
+entr7 = Entry(fen, width=30)
+entr8 = Entry(fen, width=30)
+entr9 = Entry(fen, width=30)
 entr1.grid(row = 0, column = 1)
 entr2.grid(row = 1, column = 1)
 entr3.grid(row = 2, column = 1)
@@ -153,68 +166,53 @@ entr6.grid(row = 5, column = 1)
 entr7.grid(row = 6, column = 1)
 entr8.grid(row = 7, column = 1)
 entr9.grid(row = 9, column = 1)
-ttk.Button(fen, text="Enregistrer", command=nouvelle_asso).grid(row=8, sticky=W)
-ttk.Button(fen, text="Nouveau", command=vidage).grid(row=8, column=1, sticky=W)
-ttk.Button(fen, text="Listing", command=listing).grid(row=8, column=2, sticky=W)
-ttk.Button(fen, text="Calcul", command=coef).grid(row=10, sticky=W)
-ttk.Label(message, textvariable=confirm).grid(column=0, row=11, sticky=W)
-ttk.Label(message, textvariable=liste).grid(column=1, row=11, sticky=W)
+Button(fen, text="Enregistrer", command=nouvelle_asso).grid(row=8, sticky=W)
+Button(fen, text="Nouveau", command=vidage, width=25).grid(row=8, column=1, padx=5)
+Button(fen, text="Listing", command=listing).grid(row=8, column=2, sticky=E)
+Button(fen, text="Calcul", command=coef).grid(row=10, sticky=W)
+Label(message, textvariable=confirm).grid(column=0, row=11, sticky=W)
+Label(message, textvariable=liste).grid(column=1, row=11, sticky=W)
 
-
-
-#adherent = Scale(scale, length=250, orient=HORIZONTAL, label="Adhérents",
-#                 sliderlength=20, showvalue=1, from_=-10,
-#                 to=10, tickinterval=1)
-#adherent.grid(row = 15)
-#adhesion = Scale(scale, length=250, orient=HORIZONTAL, label="Adhésion",
-#                 sliderlength=20, showvalue=1, from_=-10, to=10,
-#                 tickinterval=1)
-#adhesion.grid(row = 15, column = 1)
-#tresorerie = Scale(scale, length=250, orient=HORIZONTAL, label="Trésorerie",
-#                 sliderlength=20, showvalue=1, from_=-10, to=10,
-#                 tickinterval=1)
-#tresorerie.grid(row = 15, column = 2)
-#salarie = Scale(scale, length=250, orient=HORIZONTAL, label="Salarié(es)",
-#                 sliderlength=20, showvalue=1, from_=-10, to=10,
-#                 tickinterval=1)
-#salarie.grid(row = 16)
-#salle = Scale(scale, length=250, orient=HORIZONTAL, label="Occupation de salle",
-#                 sliderlength=20, showvalue=1, from_=-10, to=10,
-#                 tickinterval=1)
-#salle.grid(row = 16, column = 1)
-#subvention = Scale(scale, length=250, orient=HORIZONTAL, label="Sub ext",
-#                 sliderlength=20, showvalue=1, from_=-10, to=10,
-#                 tickinterval=1)
-#subvention.grid(row = 16, column = 2)
-
-adherent = ttk.Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
-adhesion = ttk.Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
-tresorerie = ttk.Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
-salarie = ttk.Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
-salle = ttk.Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
-subvention = ttk.Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
+adherent = Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
+adhesion = Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
+tresorerie = Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
+salarie = Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
+salle = Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
+subvention = Scale(scale, length=250, orient=HORIZONTAL, from_=-10, to=10)
 adherent.grid(row=16)
 adhesion.grid(row=16, column=1)
 tresorerie.grid(row=16, column=2)
 salarie.grid(row=18)
 salle.grid(row=18, column=1)
 subvention.grid(row=18, column=2)
-ttk.Label(scale, text="adhérents: ").grid(row=15, sticky=W)
-ttk.Label(scale, text="adhésion: ").grid(row=15, column=1, sticky=W)
-ttk.Label(scale, text="trésorerie: ").grid(row=15, column=2, sticky=W)
-ttk.Label(scale, text="salarié(es): ").grid(row=17, sticky=W)
-ttk.Label(scale, text="salles: ").grid(row=17, column=1, sticky=W)
-ttk.Label(scale, text="subventions ext: ").grid(row=17, column=2, sticky=W)
 
+val_adherents = DoubleVar()
+val_adhesion = DoubleVar()
+val_tresorerie = DoubleVar()
+val_salarie = DoubleVar()
+val_salle = DoubleVar()
+val_subvention = DoubleVar()
+
+Label(scale, textvariable=val_adherents).grid(row=15, sticky=W)
+Label(scale, textvariable=val_adhesion).grid(row=15, column=1, sticky=W)
+Label(scale, textvariable=val_tresorerie).grid(row=15, column=2, sticky=W)
+Label(scale, textvariable=val_salarie).grid(row=17, sticky=W)
+Label(scale, textvariable=val_salle).grid(row=17, column=1, sticky=W)
+Label(scale, textvariable=val_subvention).grid(row=17, column=2, sticky=W)
+
+root.bind('<B1-Motion>', scale_update)
+scale_val()
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 fen.columnconfigure(0, weight=1)
+fen.columnconfigure(1, weight=1)
+fen.columnconfigure(2, weight=1)
 fen.rowconfigure(0, weight=1)
-message.columnconfigure(0, weight=1)
-message.rowconfigure(0, weight=1)
-scale.columnconfigure(0, weight=1)
-scale.rowconfigure(0, weight=1)
+# message.columnconfigure(0, weight=1)
+# message.rowconfigure(0, weight=1)
+# scale.columnconfigure(0, weight=1)
+# scale.rowconfigure(0, weight=1)
 
 entr1.focus()
 
